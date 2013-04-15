0b8c060a9f19626983d74435a3816ae41aa151ec
Paul Ryan <pwryan81@hotmail.com>
2013-04-15 19:23:23 +1000

    Adding test.js and commiting in WebStorm

---
+++ test.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Paul
+ * Date: 15/04/13
+ * Time: 7:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+// Adding new js file to Github
